6ad609426099a2fad5178f57d9358f0a4c803b4e
RyanHagen77 <ryan@homeandprostudio.com>
2024-08-14 15:05:20 -0500

    Success

--- client_portal/next.config.mjs
+++ client_portal/next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
   basePath: '/client_portal',
   images: {
-    domains: ['www.enet2.com'], // Allow images from the enet2.com domain
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.enet2.com',
+        port: '',
+        pathname: '/media/**', // Adjust this path to match where your images are served from
+      },
+    ],
   },
 };
 
