2f1f7d2cbb3fc7a7d67e1e27d2f1d9771b95e975
RyanHagen77 <ryan@homeandprostudio.com>
2024-08-16 16:45:31 -0500

    Success

--- users/forms.py
+++ users/forms.py
@@ -50,7 +50,7 @@ class TaskForm(forms.ModelForm):
 
         # Setting the queryset for 'note' field
         self.fields['note'].queryset = UnifiedCommunication.objects.filter(
-            note_type__in=[UnifiedCommunication.INTERNAL, UnifiedCommunication.CONTRACT]
+            note_type__in=[UnifiedCommunication.INTERNAL, UnifiedCommunication.PORTAL]
         )
         self.fields['note'].label_from_instance = lambda obj: "{} - {}".format(obj.note_type, obj.description[:30])
 
