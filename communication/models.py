d79f04aa477b601cfa2af99374397283416d1e52
RyanHagen77 <ryan@homeandprostudio.com>
2024-10-29 11:41:40 -0500

    Success

--- communication/models.py
+++ communication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from contracts.models import Contract
 from django.conf import settings
-
+from django.core.exceptions import ValidationError  # For custom validation
 
 class UnifiedCommunication(models.Model):
     INTERNAL = 'internal'
@@ -36,22 +36,28 @@ class UnifiedCommunication(models.Model):
         ]
 
 
-
 class Task(models.Model):
     TASK_TYPES = (
         ('contract', 'Contract'),
         ('internal', 'Internal'),
     )
 
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_tasks', verbose_name="Sender")
-    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned_tasks', verbose_name="Assigned To")
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, null=True, blank=True)
-    note = models.ForeignKey(UnifiedCommunication, on_delete=models.CASCADE, verbose_name="Note", null=True, blank=True)
-    due_date = models.DateTimeField(verbose_name="Due Date")
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_tasks',
+                               verbose_name="Sender")
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned_tasks',
+                                    verbose_name="Assigned To")
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Contract")
+    note = models.ForeignKey(UnifiedCommunication, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Note")
+    due_date = models.DateTimeField(verbose_name="Due Date", db_index=True)
     description = models.TextField(verbose_name="Description")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At", db_index=True)
     is_completed = models.BooleanField(default=False, verbose_name="Is Completed")
-    type = models.CharField(max_length=10, choices=TASK_TYPES, default='internal')
+    task_type = models.CharField(max_length=10, choices=TASK_TYPES, default='internal', verbose_name="Task Type")
+
+    def clean(self):
+        super(Task, self).clean()
+        if self.task_type == 'contract' and not self.contract:
+            raise ValidationError("Contract-related tasks must have a contract assigned.")
 
     def __str__(self):
         if self.contract:
@@ -62,3 +68,6 @@ class Task(models.Model):
     class Meta:
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
+        indexes = [
+            models.Index(fields=['due_date', 'created_at']),
+        ]
