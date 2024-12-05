f40678c909ee53a7e0348c2c67881880d9f471fd
RyanHagen77 <ryan@homeandprostudio.com>
2024-12-04 21:18:29 -0600

    Success

--- wedding_day_guide/forms.py
+++ wedding_day_guide/forms.py
@@ -5,67 +5,49 @@ from .models import WeddingDayGuide
 class WeddingDayGuideForm(forms.ModelForm):
     class Meta:
         model = WeddingDayGuide
-        fields = [
-            'event_date', 'primary_contact', 'primary_email', 'primary_phone',
-            'partner_contact', 'partner_email', 'partner_phone', 'dressing_location',
-            'dressing_address', 'dressing_start_time', 'ceremony_site',
-            'ceremony_address', 'ceremony_phone', 'ceremony_start', 'ceremony_end',
-            'reception_site', 'reception_address', 'reception_phone',
-            'reception_start', 'dinner_start', 'reception_end', 'staff_table', 'photo_stop1',
-            'photo_stop2', 'photo_stop3', 'photo_stop4', 'photographer2_start_location',
-            'photographer2_start_location_address', 'photographer2_start', 'p1_attendant_of_honor',
-            'p1_attendant_qty', 'flower_attendant_qty', 'usher_qty', 'p2_attendant_of_honor',
-            'p2_attendant_qty', 'ring_bearer_qty', 'p1_parent_names',
-            'p1_sibling_names', 'p1_grandparent_names', 'p2_parent_names',
-            'p2_sibling_names', 'p2_grandparent_names',
-            'additional_photo_request1', 'additional_photo_request2',
-            'additional_photo_request3', 'additional_photo_request4',
-            'additional_photo_request5', 'video_client_names', 'wedding_story_song_title',
-            'wedding_story_song_artist', 'dance_montage_song_title', 'dance_montage_song_artist',
-            'video_special_dances', 'photo_booth_text_line1', 'photo_booth_text_line2', 'photo_booth_placement'
-        ]
-        widgets = {
-            'event_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control form-control-sm'}),
-            # Add other widgets as needed
-        }
+        fields = '__all__'  # Include all fields
 
     def __init__(self, *args, **kwargs):
-        self.strict_validation = kwargs.pop('strict_validation', False)  # Default to relaxed validation
+        self.strict_validation = kwargs.pop('strict_validation', False)
+        contract = kwargs.pop('contract', None)  # Accept the `contract` argument
         super().__init__(*args, **kwargs)
 
-        # All fields optional by default
-        for field in self.fields.values():
-            field.required = False
+        # If a contract is provided, prepopulate fields
+        if contract:
+            self.fields['event_date'].initial = contract.event_date
+            self.fields['primary_contact'].initial = contract.client.primary_contact
+            self.fields['primary_email'].initial = contract.client.primary_email
+            self.fields['primary_phone'].initial = contract.client.primary_phone1
+            self.fields['partner_contact'].initial = contract.client.partner_contact
+            self.fields['partner_email'].initial = contract.client.partner_email
+            self.fields['partner_phone'].initial = contract.client.partner_phone1
+            self.fields['ceremony_site'].initial = contract.ceremony_site
+            self.fields['reception_site'].initial = contract.reception_site
+
+        # Set required fields based on strict_validation
+        for field in self.fields:
+            self.fields[field].required = False
 
         if self.strict_validation:
-            # Enforce stricter validation only for submission
             required_fields = [
                 'event_date', 'primary_contact', 'primary_email', 'primary_phone',
                 'partner_contact', 'partner_email', 'partner_phone', 'dressing_location',
-                'dressing_address', 'dressing_start_time', 'ceremony_site',
-                'ceremony_address', 'ceremony_phone', 'ceremony_start', 'ceremony_end',
-                'reception_site', 'reception_address', 'reception_phone',
-                'reception_start', 'dinner_start', 'reception_end'
+                'dressing_start_time', 'ceremony_site', 'ceremony_start', 'ceremony_end',
+                'reception_site', 'reception_start', 'reception_end'
             ]
-            for field_name in required_fields:
-                if field_name in self.fields:
-                    self.fields[field_name].required = True
+            for field in required_fields:
+                self.fields[field].required = True
 
     def clean(self):
         cleaned_data = super().clean()
-
-        # Enforce stricter validation if strict_validation is True
         if self.strict_validation:
             required_fields = [
                 'event_date', 'primary_contact', 'primary_email', 'primary_phone',
                 'partner_contact', 'partner_email', 'partner_phone', 'dressing_location',
-                'dressing_address', 'dressing_start_time', 'ceremony_site',
-                'ceremony_address', 'ceremony_phone', 'ceremony_start', 'ceremony_end',
-                'reception_site', 'reception_address', 'reception_phone',
-                'reception_start', 'dinner_start', 'reception_end'
+                'dressing_start_time', 'ceremony_site', 'ceremony_start', 'ceremony_end',
+                'reception_site', 'reception_start', 'reception_end'
             ]
             for field in required_fields:
                 if not cleaned_data.get(field):
-                    self.add_error(field, f"{field.replace('_', ' ').title()} is required for submission.")
-
+                    self.add_error(field, f"{field.replace('_', ' ').title()} is required.")
         return cleaned_data
