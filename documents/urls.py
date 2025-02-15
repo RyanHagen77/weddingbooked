761e363011fca911b010664ab8b05cbeffa113d8
Ryan Hagen <ryan@homeandprostudio.com>
2025-02-15 11:32:44 -0600

    update forms

--- documents/urls.py
+++ documents/urls.py
@@ -9,7 +9,7 @@ path('contract_agreement/<int:contract_id>/', views.contract_agreement, name='co
 path('client_contract_agreement/<int:contract_id>/', views.client_contract_agreement,
      name='client_contract_agreement'),
 path('view_submitted_contract/<int:contract_id>/', views.view_submitted_contract, name='view_submitted_contract'),
-path('client_contract_and_rider_agreement/<int:contract_id>/', views.client_contract_and_rider_agreement,
+path('client_contract_and_rider_agreement/<int:contract_id>/', views.client_contract_agreement,
           name='client_contract_and_rider_agreement'),
 path('client_rider_agreement/<int:contract_id>/<str:rider_type>/', views.client_rider_agreement,
      name='client_rider_agreement'),
