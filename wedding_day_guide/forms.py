896c23a15aa2d299361ce849da643ff2188edb3f
Ryan Hagen <ryan@homeandprostudio.com>
2025-03-21 08:05:36 -0500

    update forms

--- wedding_day_guide/forms.py
+++ wedding_day_guide/forms.py
@@ -1,11 +1,19 @@
-# wedding_day_guide/forms.py
 from django import forms
 from .models import WeddingDayGuide
+from django.forms import TimeInput
+
+time_widget = TimeInput(format='%I:%M %p', attrs={'class': 'form-control'})
+
 
 class WeddingDayGuideForm(forms.ModelForm):
+    time_widget = TimeInput(
+        format='%H:%M',  # Use 24-hour format to match what HTML expects
+        attrs={'type': 'time', 'class': 'form-control'}
+    )
+
     class Meta:
         model = WeddingDayGuide
-        fields = '__all__'  # Include all fields
+        fields = '__all__'
         widgets = {
             'dressing_start_time': forms.TimeInput(format='%I:%M %p', attrs={'class': 'form-control'}),
             'ceremony_start': forms.TimeInput(format='%I:%M %p', attrs={'class': 'form-control'}),
@@ -18,10 +26,18 @@ class WeddingDayGuideForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.strict_validation = kwargs.pop('strict_validation', False)
-        contract = kwargs.pop('contract', None)  # Accept the `contract` argument
+        contract = kwargs.pop('contract', None)
         super().__init__(*args, **kwargs)
 
-        # If a contract is provided, prepopulate fields
+        # Ensure Django can parse 12-hour time input
+        time_fields = [
+            'dressing_start_time', 'ceremony_start', 'ceremony_end',
+            'reception_start', 'reception_end', 'dinner_start', 'photographer2_start'
+        ]
+        for field in time_fields:
+            self.fields[field].input_formats = ['%I:%M %p']
+
+        # Prepopulate fields from contract
         if contract:
             self.fields['event_date'].initial = contract.event_date
             self.fields['primary_contact'].initial = contract.client.primary_contact
@@ -33,7 +49,6 @@ class WeddingDayGuideForm(forms.ModelForm):
             self.fields['ceremony_site'].initial = contract.ceremony_site
             self.fields['reception_site'].initial = contract.reception_site
 
-        # Set required fields based on strict_validation
         for field in self.fields:
             self.fields[field].required = False
 
