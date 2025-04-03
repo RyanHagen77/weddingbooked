3aaf73c3f8ba0c9d7bf27dd73ebb39b0c6fc0af8
Ryan Hagen <ryan@homeandprostudio.com>
2025-04-03 16:00:47 -0500

    update forms

--- client_portal/src/components/WeddingDayGuideForm.tsx
+++ client_portal/src/components/WeddingDayGuideForm.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
-import StyledTimePicker from '@/components/StyledTimePicker'
+import StyledTimePicker from './TimeInput'
 
 
 interface FormData {
