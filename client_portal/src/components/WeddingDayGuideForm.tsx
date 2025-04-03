f3b6a7a6375a256e1e9ca01e2be66728f9e7fab2
Ryan Hagen <ryan@homeandprostudio.com>
2025-04-03 13:38:11 -0500

    update forms

--- client_portal/src/components/WeddingDayGuideForm.tsx
+++ client_portal/src/components/WeddingDayGuideForm.tsx
@@ -68,15 +68,16 @@ interface WeddingDayGuideFormProps {
 }
 
 const WeddingDayGuideForm: React.FC<WeddingDayGuideFormProps> = ({ contractId }) => {
-const {
-  register,
-  handleSubmit,
-  setValue,
-  formState: { errors },
-} = useForm<FormData>({
-  defaultValues: {} as FormData,
-  mode: "onBlur", // or "onChange" if you want real-time validation feedback
-});
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {} as FormData,
+    mode: "onBlur",
+  });
 
 
   const [isSubmitted, setIsSubmitted] = useState(false);
