a94c436f0e7ae296adc8aad9ab9501f906f0e1fe
Ryan Hagen <ryan@homeandprostudio.com>
2025-07-22 15:21:27 -0500

    search pagnation update, wdg required fields.

--- wedding_day_guide/forms.py
+++ wedding_day_guide/forms.py
@@ -54,10 +54,13 @@ class WeddingDayGuideForm(forms.ModelForm):
 
         if self.strict_validation:
             required_fields = [
-                'event_date', 'primary_contact', 'primary_email', 'primary_phone',
-                'partner_contact', 'partner_email', 'partner_phone', 'dressing_location',
-                'dressing_start_time', 'ceremony_site', 'ceremony_start', 'ceremony_end',
-                'reception_site', 'reception_start', 'reception_end'
+                'event_date',
+                'primary_contact',
+                'primary_email',
+                'primary_phone',
+                'partner_contact',
+                'partner_email',
+                'partner_phone',
             ]
             for field in required_fields:
                 self.fields[field].required = True
